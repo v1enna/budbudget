1705d5f4b3564c76bee958f1e6a0e648ab868d77
v1enna <19vienna20@gmail.com>
2021-02-11 23:01:08 +0100

    Add logout button component, for real

---
+++ BudBudget.WEB/src/components/LogoutButton.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Button } from 'antd'
+import { useLoginContext } from '../contexts/LoginContext'
+
+export default function LogoutButton() {
+	const contextType = useLoginContext();
+
+	function tryLogout() {
+		if(contextType.isLoggedIn && contextType.setIsLoggedIn) {
+			contextType.setIsLoggedIn(false);
+			document.cookie = "sid=";
+		}
+	}
+
+	return (
+		<Button type="primary" danger onClick={tryLogout}>
+			Esci
+		</Button>
+	)
+}
