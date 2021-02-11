eebf30e15beaaf7fd87e4bee5a7c55952d1bfbea
v1enna <19vienna20@gmail.com>
2021-02-12 00:36:02 +0100

    Generate random content for both the chart and the table if no data is available

--- BudBudget.WEB/src/components/CategoryBarChart.tsx
+++ BudBudget.WEB/src/components/CategoryBarChart.tsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import { Category } from '..models/Category'
+import React from "react";
+import { Category } from "../models/Category";
+import { Entry } from "../models/Entry";
+import { Bar } from "@ant-design/charts";
 
 export default function CategoryBarChart() {
+	const data = [
+		// should be sorted
+		{
+			name: "Mezzi",
+			value: 120
+		},
+		{
+			name: "Palestra",
+			value: 75
+		},
+		{
+			name: "Vestiti",
+			value: 100
+		},
+		{
+			name: "Cibo",
+			value: 95
+		},
+	];
+
+	const config = {
+		data: data,
+		xField: "value",
+		yField: "name",
+		seriesField: 'name'
+	}
+
 	return (
 		<div>
-			
+			<Bar {...config} />
 		</div>
 	)
 }
--- BudBudget.WEB/src/containers/DashboardContainer.tsx
+++ BudBudget.WEB/src/containers/DashboardContainer.tsx
@@ -1,15 +1,66 @@
-import React from 'react'
+import React from 'react';
 import { 
 	Card, 
 	Col, 
 	Row,
-	Layout
-} from 'antd'
-import "./DashboardContainer.css"
+	Layout,
+	Table
+} from 'antd';
+import "./DashboardContainer.css";
+import CategoryBarChart from "../components/CategoryBarChart";
+import { Bar } from "@ant-design/charts";
 
 const { Header, Content } = Layout;
 
+interface CategoryValues {
+	categoryName: string,
+	categoryValue: number
+}
+
 export default function DashboardContainer() {
+	function createDataSource() : CategoryValues[] {
+		// somehow fetch entries by category here
+		var data : CategoryValues[] = [];
+
+		// if no entries found, fill with example data
+		if(data.length == 0) {
+			for(let i = 0; i < 10; i++) {
+				data.push({
+					categoryName: require('randomstring').generate({
+						length: 10,
+						charset: 'alphabetic',
+						readable: true
+					}),
+					categoryValue: Math.floor(Math.random() * 149)
+				});
+			}
+		}
+
+		return data;
+	};
+
+	const dataSource = createDataSource();
+
+	const columns = [
+		{
+			title: "Categoria",
+			dataIndex: "categoryName",
+			key: "categoryName"
+		},
+		{
+			title: "Valore",
+			dataIndex: "categoryValue",
+			key: "categoryValue"
+		}
+	];
+
+	const chartConfig = {
+		data: dataSource,
+		xField: "categoryValue",
+		yField: "categoryName",
+		seriesField: 'categoryName'
+	}
+
 	return (
 		<div>
 			<Layout>
@@ -22,19 +73,29 @@ export default function DashboardContainer() {
 					<Row className="content-row">
 						<Col span={24}>
 							<Card title={"Dove ho speso di più?"}>
-							&nbsp;
+								<Bar {...chartConfig} />
 							</Card>
 						</Col>
 					</Row>
 					<Row gutter={16}>
 						<Col span={12}>
 							<Card title={"Spese per categoria"}>
-								&nbsp;
+								<Table columns={columns} dataSource={dataSource}></Table>
 							</Card>
 						</Col>
 						<Col span={12}>
 							<Card title={"Saldo complessivo"}>
-								&nbsp;
+								<div className="balance">
+									<div className="money">
+										<strong>Bilancio: <span>2917€</span></strong>
+									</div>
+									<div className="money_in">
+										<strong>Entrate del mese: <span>+1203€</span></strong>
+									</div>
+									<div className="money_out">
+										<strong>Uscite del mese: <span>-712€</span></strong>
+									</div>
+								</div>
 							</Card>
 						</Col>
 					</Row>
